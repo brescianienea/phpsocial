3cee32d933ca5f6ea89754f5594e6cfd6fa6c853
Federico Tamburini <federico.tamburini@studio.unibo.it>
2024-01-09 23:48:15 +0100

    added user registration with database

--- home/friends.php
+++ home/friends.php
@@ -1,1 +1,10 @@
-Friends
+<?php
+ob_start();
+include (__DIR__ . "/../Database.php");
+include (__DIR__ . "/../Friend.php");
+$data = ob_get_clean();
+$asd = $Friend->getFriends("5");
+while ($row = mysqli_fetch_assoc($result)) {
+	echo $row['user_id'];
+}
+?>
--- query/login/register.php
+++ query/login/register.php
@@ -1,4 +1,7 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $ds = DIRECTORY_SEPARATOR;
 $base_dir = realpath(dirname(__FILE__) . $ds . '..' . $ds . '..') . $ds;
 require_once("{$base_dir}dbcon.php");
@@ -7,24 +10,78 @@ require_once("{$base_dir}dbcon.php");
 $db = $conn;
 const tableName = 'users';
 $userData = $_POST;
-loginUser($db, $userData);
-function loginUser($db, $userData)
+registerUser($db, $userData);
+function registerUser($db, $userData)
 {
+
+    $mail = $userData['mail'];
+    $day = $userData['day'];
+    $month = $userData['month'];
+    $year = $userData['year'];
     $username = $userData['username'];
     $password = $userData['password'];
-
-    if (!empty($username) && !empty($password)) {
-
-        $query = "SELECT username, password FROM " . tableName;
-        $query .= " WHERE username = '$username' AND password = '$password'";
-        $result = $db->query($query);
-        if ($result->num_rows > 0) {
-            echo "success";
+    $response = [];
+    try{
+        if (!empty($username) && !empty($password) && !empty($day) && !empty($month) && !empty($year)) {
+            $date = $year . "-" . $month . "-" . $day;
+            $dateTimeStamp = strtotime($date);
+            $currentTimestamp = time();
+            if (isValidDate($month, $day, $year) && $dateTimeStamp > $currentTimestamp) {
+                $currYear = date("Y");
+                $currMonth = date("M");
+                $currDay = date("D");
+                $age = $currYear - $year;
+                if($age == 16) {
+                    if($currMonth > $month || ($currMonth == $month && $currDay > $day)) {
+                        age--;
+                    }
+                }
+                if($age >= 16) {
+                    $query = "SELECT username" . tableName;
+                    $query .= " WHERE username = '$username'";
+                    $result = $db->query($query);
+                    if ($result->num_rows > 0) {
+                        $response['message'] = "Username already in use.";
+                    } else {
+                        $query = "SELECT email" . "members";
+                        $query .= " WHERE email = '$mail'"
+                        $result = $db->query($query);
+                        if($result->num_rows > 0) {
+                            $response['message'] = "Email already in use.";
+                        } else {
+                            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+                            $query = "INSERT INTO `users` (`username`, `password`, `admin`) VALUES ('$username', '$hashedPassword', 0)"
+                            $db->query($query);
+                            $query = "SELECT user_id" . tableName;
+                            $query .= " WHERE username = '$username'";
+                            $result = $db->query($query);
+                            if($result->num_rows != 1) {
+                                $response['message'] = "Something went wrong.";
+                            } else {
+                                $resultArr = $result->fetch_array(MYSQLI_BOTH);
+                                $memberID = $resultArr['user_id'];
+                                $dateFinal = date("Y-m-d", strtotime('$year' . '-' . '$month' . '-' . '$day'));
+                                $query = "INSERT INTO `members` (`member_id`, `email`, `birthdate`) VALUES ('$memberID', '$mail', '$dateFinal')"
+                                $db->query($query);
+                            }
+                            $response = array_merge($response, $result->fetch_array(MYSQLI_BOTH));
+                        }
+                    }
+                } else {
+                    $response['message'] = "The date is not valid.\nNot 16 Years old.";
+                }
+            } else {
+                $response['message'] = "The date is not valid.";
+            }
         } else {
-            echo "Wrong username and password";
+            $response['message'] = "All Fields are required";
         }
-
-    } else {
-        echo "All Fields are required";
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n"; 
     }
+    
+
+
+
+    echo json_encode($response);
 }

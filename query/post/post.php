ee5a90e45df9db162c914a8ad6c811d127320e74
Federico Tamburini <federico.tamburini@studio.unibo.it>
2024-01-12 23:53:37 +0100

    fixed some bugs, added select by post id

--- query/post/post.php
+++ query/post/post.php
@@ -15,7 +15,7 @@ class Post
             $userID = $userData;
             $response = [];
             if (!empty($userID)) {
-                $query = "SELECT post_id, content, datetime_posted FROM post";
+                $query = "SELECT post_id, content, datetime_posted, tenor_tag, game_tag FROM post";
                 $query .= " WHERE member_id = " . $userID;
                 $result = $db->query($query);
                 if ($result->num_rows > 0) {
@@ -33,5 +33,34 @@ class Post
         }
     }
 
-
+    static function getPostsByPostID($userData)
+    {
+        try {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $ds = DIRECTORY_SEPARATOR;
+            $base_dir = realpath(dirname(__FILE__) . $ds . '..' . $ds . '..') . $ds;
+            require("{$base_dir}dbcon.php");
+            $db = $conn;
+            $postID = $userData;
+            $response = [];
+            if (!empty($userID)) {
+                $query = "SELECT member_id, content, datetime_posted, tenor_tag, game_tag FROM post";
+                $query .= " WHERE post_id = " . $userID;
+                $result = $db->query($query);
+                if ($result->num_rows > 0) {
+                    $result = $result->fetch_assoc();
+                    return $result;
+                } else {
+                    return null;
+                }
+    
+            } else {
+                return null;
+            }
+        } catch (Exception $e) {
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+        }
+    }
 }

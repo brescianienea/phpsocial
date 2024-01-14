220797fc3a83f42d09a7db8cbe59edf52536637f
Enea Bresciani <enea.bresciani@gmail.com>
2024-01-14 23:30:07 +0100

    partial commit

---
+++ social/requests.php
@@ -0,0 +1,15 @@
+<section class="search-container">
+    <form>
+        <label for="search" hidden>Search a user</label>
+        <input id="search" type="text" name="search" placeholder="Serach a user">
+    </form>
+</section>
+<section class="requests-container">
+    <section class="received">
+
+
+    </section>
+    <section class="sended">
+
+    </section>
+</section>

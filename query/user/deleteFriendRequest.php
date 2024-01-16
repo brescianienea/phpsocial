5a878cf849df9a36f33bafc8a79ccf6cae1db1d9
Federico Tamburini <federico.tamburini@studio.unibo.it>
2024-01-16 21:50:26 +0100

    added way to remove friends and friend requests

---
+++ query/user/deleteFriendRequest.php
@@ -0,0 +1,46 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+$ds = DIRECTORY_SEPARATOR;
+$base_dir = realpath(dirname(__FILE__) . $ds . '..' . $ds . '..') . $ds;
+require_once("{$base_dir}dbcon.php");
+?>
+<?php
+$db = $conn;
+$userData = $_POST;
+addFriend($db, $userData);
+
+function addFriend($db, $userData) {
+    $sender = $userData['sender'];
+    $receiver = $userData['receiver'];
+    $response = [];
+
+    try {
+        if (!empty($sender) && !empty($receiver)) {
+            $query = "SELECT sender FROM friend_requests";
+            $query .= " WHERE sender = '$sender'";
+            $query .= " AND receiver = '$receiver'";
+            $query .= " OR sender = '$receiver'";
+            $query .= " AND receiver = '$sender'";
+            $result = $db->query($query);
+            if ($result->num_rows > 0) {
+                $query = "DELETE FROM friend_requests";
+                $query .= " WHERE sender = '$sender'";
+                $query .= " AND receiver = '$receiver'";
+                $query .= " OR sender = '$receiver'";
+                $query .= " AND receiver = '$sender'";
+                $db->query($query);
+                $response['message'] = "success";
+                //$response = array_merge($response, $result->fetch_array(MYSQLI_BOTH));
+            } else {
+                $response['message'] = "Friend request doesn't exist.";
+            }
+        } else {
+            $response['message'] = "Some required fields are empty";
+        }
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
+    }
+    echo json_encode($response);
+}
